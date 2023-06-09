c8c67e33020bf05e80829f066da0e9462409f81d
Ebru Şara Bağca <ebru.sara123@outlook.com>
2023-06-09 12:22:10 +0300

    Necessary files added

---
+++ lib/pages/pages.dart
@@ -0,0 +1,6 @@
+export 'forgot_password/forgot_password.dart';
+export 'forgot_password/forgot_password_verification.dart';
+export 'login/login_page.dart';
+export 'profile/profile_page.dart';
+export 'register/registration_page.dart';
+export 'welcome/welcome_screen.dart';
---
+++ lib/style/styles.dart
@@ -0,0 +1,3 @@
+export 'theme/theme_helper.dart';
+export 'theme/theme_util.dart';
+export 'widget/header_widget.dart';
